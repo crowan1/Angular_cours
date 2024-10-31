3cf89aa9ee9a0d1b6945a85fe7a8b9f82aedaed2
crowan1 <144345726+crowan1@users.noreply.github.com>
2024-10-31 11:01:23 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,12 @@ import { AccueilComponent } from './accueil/accueil.component';
 import { ConnexionComponent } from './connexion/connexion.component';
 import { Page404Component } from './page404/page404.component';
 import { utilisateurConnecteGuard } from './services/utilisateur-connecte.guard';
-import { BackOfficeComponent } from './back-office/back-office.component';
 
 
 export const routes: Routes = [
   {
     path: 'accueil',
     component: AccueilComponent,
-    canActivate: [utilisateurConnecteGuard],
-  },
-  {
-    path : 'administration',
-    component : BackOfficeComponent,
     canActivate: [utilisateurConnecteGuard],
   },
   { path: 'connexion', component: ConnexionComponent },
